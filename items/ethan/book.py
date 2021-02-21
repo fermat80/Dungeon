210b3f3489d0c4f459f53c54ca89d1ca83411fec
fermat8 <fermat@comcast.net>
2021-02-21 04:40:38 +0000

    Fixed a bug.

--- items/ethan/book.py
+++ items/ethan/book.py
@@ -10,29 +10,29 @@ class Book(Item):
 
     self.is_open = False
 
-    def is_named(self, name):
-      return name == 'biome_book'
+  def is_named(self, name):
+    return name == 'biome_book'
 
 
 
-    def get_description(self):
-      if self.is_open:
-        return self.description + 'The book is open.'
-      return self.description + 'The book is closed.'
+  def get_description(self):
+    if self.is_open:
+      return self.description + 'The book is open.'
+    return self.description + 'The book is closed.'
 
 
 
-    def do(self, player, command):
-      if command == 'open book':      
-        if not self.is_open:
-          print('You open the book about biomes and information pours into your head.')
-          self.name = 'A open book'
-          self.is_open = True
-        else:
-          print('You close the strange book and the flow of information stops coming.')
-          self.name = 'A closed book'
-          self.is_open = False
-        return True
+  def do(self, player, command):
+    if command == 'open book':      
+      if not self.is_open:
+        print('You open the book about biomes and information pours into your head.')
+        self.name = 'A open book'
+        self.is_open = True
+      else:
+        print('You close the strange book and the flow of information stops coming.')
+        self.name = 'A closed book'
+        self.is_open = False
+      return True
 
 
 
