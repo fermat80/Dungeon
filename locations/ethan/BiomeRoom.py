5d6ba94c881a394fa79df59c84534d311b554e39
fermat8 <fermat@comcast.net>
2021-02-21 04:45:27 +0000

    Merge again!

--- locations/ethan/BiomeRoom.py
+++ locations/ethan/BiomeRoom.py
@@ -1,8 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from locations import Location
 
 def locations():
   return [BiomeRoom(), ]
+=======
+from location import Location
+from items.ethan.book import Book
+>>>>>>> origin/master
 =======
 from location import Location
 from items.ethan.book import Book
@@ -15,7 +20,11 @@ class BiomeRoom(Location):
     self.name = 'A room with many different ecosystems.'
     self.description = 'You have found the biome room, it is a place with many different creatures in different enviornments.'
 <<<<<<< HEAD
+<<<<<<< HEAD
     
+=======
+    self.contents = [Book()]
+>>>>>>> origin/master
 =======
     self.contents = [Book()]
 >>>>>>> origin/master
