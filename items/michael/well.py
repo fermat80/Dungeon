a97ccbf7877ebe9c50f62083bd00ca2d705d5c7e
fermat8 <fermat@comcast.net>
2021-02-22 13:57:39 +0000

    Merge

---
+++ items/michael/well.py
@@ -0,0 +1,21 @@
+from item import Item
+
+class Well(Item):
+
+  Key=False
+
+  def __init__(self):
+    self.id = 'Michael.Well'
+
+    self.name = 'Well'
+
+    self.description = " The water in the well is wet and clear. In the bucket of the well, there is a key. You can pick up the key."
+
+  def do(self, player, command):
+    if command == 'pick up key' or command=='pick up':
+      Key=True
+      return True      
+    return False
+
+  def is_named(self, name):
+    return name == 'Well'
---
+++ locations/michael/building.py
@@ -0,0 +1,52 @@
+from location import Location
+from items.michael.notes import Note1
+from items.michael.notes import Note2
+from items.michael.well import Well
+
+
+
+class Basement(Location):
+
+  def __init__(self):
+    self.id = 'basement'
+    self.name = "The Basement"
+    self.description = "The walls are crumbly and you hear bats squeaking. There is a note on the floor. There are two doorways. One leads North and one leads South."    
+    self.contents = [Note1()]
+
+  def do(self, player, command):
+    if (command == 'n' or command == 'north'):
+      self.move_player(player, 'well_room')
+      return True; 
+    if (command == 's' or command == 'south'):
+      self.move_player(player, 'hallway')    
+    return False;
+
+class Well_Room(Location):
+
+  def __init__(self):
+    self.id = 'well'
+    self.name = "Well Room"
+    self.description = 'The room is empty, except for a well. There is a note on the side of the well.'
+    self.contents = [Note2(), Well()]
+
+  def do(self, player, command):
+    if (command == 's' or command == 'south'):
+      self.move_player(player, 'basement')
+      return True;     
+    return False;
+
+class Hallway(Location):
+
+  Door=False
+
+  def __init__(self):
+    self.id = 'hallway'
+    self.name = "Hallway"
+    self.description = 'A hallway with a door on the other side. The door seems to need a key.'
+    self.contents = []
+
+  def do(self, player, command):
+    if (command == 'n' or command == 'north'):
+      self.move_player(player, 'basement')
+      return True;     
+    return False;
