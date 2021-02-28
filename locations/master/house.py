f4eeb2c64557bf4b2df30b82736b6867bedb0796
fermat8 <fermat@comcast.net>
2021-02-28 18:49:55 +0000

    Merge

--- locations/master/house.py
+++ locations/master/house.py
@@ -3,6 +3,9 @@ from items.master.box import Box
 from items.master.keys import BoxKey
 from items.master.letter import Letter
 from items.master.games import HangmanGame
+from items.master.doors import TwoSumDoor
+from items.master.keys import TwoSumKey
+from items.master.creatures import Dragon
 
 class Foyer(Location):
 
@@ -23,11 +26,88 @@ class GameRoom(Location):
   def __init__(self):
     self.id = 'master_gameroom'
     self.name = "Dungeon Master's Game Room"
-    self.description = 'This is the famed game room of the dungeon master!  There is an open doorway leading south.'
+    self.description = 'This is the famed game room of the dungeon master!  There is an open doorway leading south.  The challenge area is to the east.'
     self.contents = [HangmanGame(), Box()]
 
   def do(self, player, command):
+
     if (command == 's' or command == 'south'):
       self.move_player(player, 'master_foyer')
+      return True;     
+
+    if (command == 'e' or command == 'east'):
+      self.move_player(player, 'master_challengearea')
+      return True;     
+
+    return False;
+
+class ChallengeArea(Location):
+
+  def __init__(self):
+    self.id = 'master_challengearea'
+    self.name = 'Challenge Area'
+    self.description = 'This is the entrance to the coding challenge area!  The easy challenges are north, medium challenges east, and hard challenges south (beware, there be dragons).  The game room is to the west.'
+    self.contents = []
+
+  def do(self, player, command):
+
+    if (command == 'w' or command == 'west'):
+      self.move_player(player, 'master_gameroom')
+      return True;     
+
+    if (command == 'n' or command == 'north'):
+      self.move_player(player, 'master_easychallengeroom')
+      return True;     
+
+    if (command == 'e' or command == 'east'):
+      self.move_player(player, 'master_mediumchallengeroom')
+      return True;     
+
+    if (command == 's' or command == 'south'):
+      self.move_player(player, 'master_hardchallengeroom')
+      return True;     
+
+    return False;
+
+class EasyChallengeRoom(Location):
+
+  def __init__(self):
+    self.id = 'master_easychallengeroom'
+    self.name = 'Easy Challenge Room'
+    self.description = 'This is the easy challenge room!  The challenge area is to the south.'
+    #self.contents = [TwoSumDoor(), TwoSumKey()]
+    self.contents = [TwoSumDoor()]
+
+  def do(self, player, command):
+    if (command == 's' or command == 'south'):
+      self.move_player(player, 'master_challengearea')
+      return True;     
+    return False;
+
+class MediumChallengeRoom(Location):
+
+  def __init__(self):
+    self.id = 'master_mediumchallengeroom'
+    self.name = 'Medium Challenge Room'
+    self.description = 'This is the medium challenge room!  The challenge area is to the west.'
+    self.contents = []
+
+  def do(self, player, command):
+    if (command == 'w' or command == 'west'):
+      self.move_player(player, 'master_challengearea')
+      return True;     
+    return False;
+
+class HardChallengeRoom(Location):
+
+  def __init__(self):
+    self.id = 'master_hardchallengeroom'
+    self.name = 'Hard Challenge Room'
+    self.description = 'This is the hard challenge room!  The challenge area is to the north.'
+    self.contents = [Dragon()]
+
+  def do(self, player, command):
+    if (command == 'n' or command == 'north'):
+      self.move_player(player, 'master_challengearea')
       return True;     
     return False;
--- locations/master/load.py
+++ locations/master/load.py
@@ -1,5 +1,9 @@
 from .house import Foyer
 from .house import GameRoom
+from .house import EasyChallengeRoom
+from .house import MediumChallengeRoom
+from .house import HardChallengeRoom
+from .house import ChallengeArea
 
 def locations():
-  return [Foyer(), GameRoom()]
+  return [Foyer(), GameRoom(), EasyChallengeRoom(), MediumChallengeRoom(), HardChallengeRoom(), ChallengeArea()]
