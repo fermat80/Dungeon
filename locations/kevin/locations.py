c7f41a9492205236fd7abb83f917396fb4ef918c
fermat8 <fermat@comcast.net>
2021-03-07 18:37:42 +0000

    Merge

--- locations/kevin/locations.py
+++ locations/kevin/locations.py
@@ -2,6 +2,7 @@ from location import Location
 from items.kevin.cat import Cat
 from items.kevin.fidget import FidgetCube
 from items.kevin.sign import Sign
+from items.kevin.spider import GiantSpider
 
 class HobbitHole(Location):
 
@@ -46,7 +47,7 @@ class DarkForest(Location):
     self.id = 'dark_forest'
     self.name = 'Dark Forest'
     self.description = 'A dark and eerie forest, mysterious sounds surround you. You have heard many tales of people who never return from these woods.'
-    self.contents = []
+    self.contents = [GiantSpider()]
   
   def do(self, player, command):
     if (command == 'n' or command == 'north'):
