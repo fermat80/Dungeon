cc6a5bad2ace5f825e86f476e31047d33d26c117
fermat8 <fermat@comcast.net>
2021-03-13 19:02:47 +0000

    Merge

--- items/michael/key.py
+++ items/michael/key.py
@@ -1,1 +1,21 @@
 from item import Item
+
+class Key(Item):
+
+  def __init__(self):
+    self.id = 'michael.key'
+    self.name = 'Key'
+    self.description = " A large and rusted key. "
+
+  def get_description(self):
+    s = self.description
+    return s
+
+  def do(self, player, command):
+    if command == 'pick up key' or command=='pick up':
+      self.Key=True
+      return True      
+    return False
+
+  def is_named(self, name):
+    return name == 'key'
