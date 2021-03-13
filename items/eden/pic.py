0e2f9d71213f1dd2f967a8193a33f1c0b496cf15
fermat8 <fermat@comcast.net>
2021-03-13 18:53:37 +0000

    Merge

--- items/eden/pic.py
+++ items/eden/pic.py
@@ -5,7 +5,10 @@ from item import Item
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> origin/master
 =======
 >>>>>>> origin/master
 =======
@@ -29,7 +32,12 @@ class Pic(Item):
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         return name == 'pic'
+=======
+        #return name == 'pic'
+        return name in ['pic', 'picture']
+>>>>>>> origin/master
 =======
         #return name == 'pic'
         return name in ['pic', 'picture']
