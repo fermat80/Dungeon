712df56a4b3158372ea2e7264f9e227949fc7c28
fermat8 <>
2021-04-25 19:06:47 +0000

    Merge

--- locations/ethan/BiomeRoom.py
+++ locations/ethan/BiomeRoom.py
@@ -1,10 +1,12 @@
 from location import Location
 from items.ethan.book import Book
 from items.ethan.jacket import jacket 
+from items.ethan.key import Key 
 class BiomeRoom(Location):
 
   def __init__(self):
     self.id = 'Biome_Room'
     self.name = 'A room with many different ecosystems.'
     self.description = 'You have found the biome room, it is a place with many different creatures in different enviornments.'
-    self.contents = [Book(), jacket()]
+    self.contents = [Book(), jacket(), Key()]
+
