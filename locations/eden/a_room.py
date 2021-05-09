750581c0d9ad8374cc5ad1b5a652ace2a5d11aa2
fermat8 <>
2021-05-09 18:29:36 +0000

    Merge

--- locations/eden/a_room.py
+++ locations/eden/a_room.py
@@ -1,12 +1,14 @@
 from location import Location 
 from items.eden.pic import Pic
+from items.eden.master_keys import TwoSumKey
+from items.eden.master_keys import ShuffleKey
 
 class A_room(Location):
   def __init__(self):
     self.id='a_room'
     self.name='a wizard studying room'
-    self.description='room with light and picture on wall westsouthwest is a duelling area.'
-    self.contents=[Pic()]
+    self.description='Room with light and picture on wall. westsouthwest is a duelling area.'
+    self.contents=[Pic(), TwoSumKey(), ShuffleKey()]
   def do(self,player,command):
     if command in ["westsouthwest",'wsw']:
       return self.move_player(player,"B_room")
