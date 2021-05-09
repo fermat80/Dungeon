059d681605cf56350db85213f5e8d5c04853b339
fermat8 <>
2021-05-09 18:29:17 +0000

    Merge

--- locations/wesley/volcano.py
+++ locations/wesley/volcano.py
@@ -1,4 +1,6 @@
 from location import Location
+from items.wesley.master_keys import TwoSumKey
+from items.wesley.master_keys import ShuffleKey
 
 class Volcano(Location):
 
@@ -6,4 +8,4 @@ class Volcano(Location):
     self.id = 'Volcano_Biome'
     self.name = "Volcano"
     self.description = "You find yourself in a Volcano. On the ground you see a book covered with ash, you will need a key to open it."
-    self.contents = []
+    self.contents = [TwoSumKey(),ShuffleKey()]
